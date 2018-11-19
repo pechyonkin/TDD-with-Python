19d62c953ec8c061e52948e0aeed060b58579fad
Maxim Pechyonkin <maxim.pechyonkin@gmail.com>
2018-11-19 11:28:56 +0800

    change tabs from 4 to 2 spaces

--- lists/tests.py
+++ lists/tests.py
@@ -7,14 +7,14 @@ from lists.views import home_page
 
 class HomePageTest(TestCase):
 
-    def test_root_resolves_to_home_page_view(self):
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
+  def test_root_resolves_to_home_page_view(self):
+    found = resolve('/')
+    self.assertEqual(found.func, home_page)
 
-    def test_home_page_returns_correct_http(self):
-        request = HttpRequest()
-        response = home_page(request)
-        html = response.content.decode('utf-8')
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>To-Do lists</title>', html)
-        self.assertTrue(html.endswith('</html>'))
+  def test_home_page_returns_correct_http(self):
+    request = HttpRequest()
+    response = home_page(request)
+    html = response.content.decode('utf-8')
+    self.assertTrue(html.startswith('<html>'))
+    self.assertIn('<title>To-Do lists</title>', html)
+    self.assertTrue(html.endswith('</html>'))
